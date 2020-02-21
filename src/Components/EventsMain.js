6dba39ad458cd1cf5e0f13450dfa2706d0f065c5
Leah Zhao <leahtingxuezhao@gmail.com>
2020-02-20 23:35:55 -0700

    events done

--- src/Components/EventsMain.js
+++ src/Components/EventsMain.js
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { Component } from "react";
 
-function EventsMain(props) {
-  return <div>Events</div>;
+class EventsMain extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      events: []
+    };
+  }
+
+  async componentDidMount() {
+    const url =
+      "https://www.eventbriteapi.com/v3/organizations/416793458295/events/?token=VX7OEYIVZAMNFZG44SZ5";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ events: data.events });
+  }
+
+  render() {
+    const { events } = this.state;
+    let eventList = events.map(element => {
+      return (
+        <div className="event-box">
+          <div>{element.name.text}</div>
+          <img
+            src={element.logo.original.url}
+            alt="event-image"
+            className="event-image"
+          ></img>
+          <div>{element.description.text}</div>
+          <a href={element.url}>Check ticket</a>
+        </div>
+      );
+    });
+
+    console.log("events :", events);
+    return <div>{eventList}</div>;
+  }
 }
 
 export default EventsMain;
